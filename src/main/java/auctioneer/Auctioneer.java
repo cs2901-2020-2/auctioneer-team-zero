7bb1265291e1f4804429ffcc70ed6479eab31334
alonso804 <alonsobs804@gmail.com>
2020-10-19 14:44:22 -0500

    initial commit

---
+++ src/main/java/auctioneer/Auctioneer.java
@@ -0,0 +1,18 @@
+package auctioneer;
+
+public class Auctioneer implements Subject {
+    @Override
+    public void registerObserver(Observer o) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+}
---
+++ src/main/java/auctioneer/Bidder.java
@@ -0,0 +1,8 @@
+package auctioneer;
+
+public class Bidder implements Observer {
+    @Override
+    public void update(Object data) {
+
+    }
+}
