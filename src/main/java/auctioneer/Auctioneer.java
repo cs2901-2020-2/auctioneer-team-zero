50ccaf872e81383bf1148575d2fac17ae7d763fb
alonso804 <alonsobs804@gmail.com>
2020-10-19 15:47:34 -0500

    add some code

--- src/main/java/auctioneer/Auctioneer.java
+++ src/main/java/auctioneer/Auctioneer.java
@@ -1,18 +1,39 @@
 package auctioneer;
 
+import java.util.*;
+import java.util.logging.Logger;
+
 public class Auctioneer implements Subject {
+    static private final Logger logger = Logger.getLogger(Auctioneer.class.getName());
+    private List<Observer> bidders;
+    private double current_bid;
+
     @Override
     public void registerObserver(Observer o) {
-
+        bidders.add(o);
     }
 
     @Override
     public void removeObserver(Observer o) {
-
+        bidders.remove(o);
     }
 
     @Override
     public void notifyObservers() {
+        for(var bidder : bidders) {
+            bidder.update(this.current_bid);
+        }
+    }
 
+    public void getBid(double bid) {
+        this.current_bid = bid;
+
+        notifyObservers();
+    }
+
+    public void updateBid(double bid) {
+        this.current_bid = bid;
+
+        notifyObservers();
     }
 }
--- src/main/java/auctioneer/Bidder.java
+++ src/main/java/auctioneer/Bidder.java
@@ -1,8 +1,20 @@
 package auctioneer;
 
+import java.util.*;
+import java.util.logging.Logger;
+
 public class Bidder implements Observer {
+    static private final Logger logger = Logger.getLogger(Bidder.class.getName());
+    private double total;
+    private double current_bid;
+
     @Override
     public void update(Object data) {
+        current_bid = (double)data;
+    }
 
+    public boolean bid() {
+
+        return true;
     }
 }
